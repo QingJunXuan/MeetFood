f2e86662069ae459363dab3521f8f07d66fdecf2
QingJunXuan <junqinglx@163.com>
2018-12-13 12:50:58 +0800

    登录/注册/返回所有用户-1
    未上传数据库

---
+++ back-end/src/main/java/hello/User.java
@@ -0,0 +1,69 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private Integer type = 0;
+    private String name;
+    private String email;
+    private String password;
+    private String bio;//简介
+    private String photo;//头像
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+    public Integer getType() {
+        return type;
+    }
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
+
---
+++ back-end/src/main/java/hello/UserRepository.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import hello.User;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    //@Query(value = "from User u where u.email=:email")
+    List<User> findByEmail(@Param("email") String email);
+    List<User> findByEmailAndPassword(@Param("email") String email,
+                                    @Param("password") String password);
+}
