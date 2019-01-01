3dcbaa10a2a15e46b6a42f8e362d6757d8dc54a1
国琳 <guolin19980718@163.com>
2019-01-01 23:43:44 +0800

    修改图片尺寸

--- frontend/src/views/Guest/Order/GuestOrderDone.vue
+++ frontend/src/views/Guest/Order/GuestOrderDone.vue
@@ -14,7 +14,9 @@
                      <div class="box">
                         <el-col :span="8" v-for="(item, i) in Favorite" :key="i">
                             <el-card :body-style="{ padding: '0px' }">
-                             <img :src="item.img" class="image">
+                             <div class="recommend_pic_box" style="width:100%;height:300px;overflow:hidden;">
+                             <img :src="item.img" class="image" style="height:600px;width:900px">
+                                </div>
                          <div style="padding: 60px;">
                             <span>{{item.favoriteID}}</span>
                             <div class="text item">
